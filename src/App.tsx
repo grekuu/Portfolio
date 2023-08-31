bf4835d2799feefa0928ed9240f6811f23ab0f4b
grekuu <gracjanprusik@gmail.com>
2023-08-31 10:36:38 +0200

    feat: components setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import About from "./components/About/About";
+import Contact from "./components/Contact/contact";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Portfolio from "./components/Portfolio/Portfolio";
 import "./index.scss";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
