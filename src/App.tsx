77444db0f910b3682fda88c8f364ea21e045f604
grekuu <gracjanprusik@gmail.com>
2023-08-31 11:13:24 +0200

    feat(Header): desktop component done

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import About from "./components/About/About";
 import Contact from "./components/Contact/contact";
 import Footer from "./components/Footer/Footer";
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,24 @@
 import "./header.scss";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Navbar>
+      <Container className="header-container">
+        <Navbar.Brand href="#" className="name-logo">
+          greku
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end links-container">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Projects</a>
+          <a href="#">Contact</a>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
