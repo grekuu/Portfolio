8ea8ca02c896c96838faa6f13179f574cd504842
grekuu <gracjanprusik@gmail.com>
2023-09-01 14:49:58 +0200

    feat(Header): mobile navigfation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,43 @@
+import { useState } from "react";
 import "./header.scss";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
 const Header = () => {
+  const [showMobileNav, setShowMobileNav] = useState(false);
   return (
-    <Navbar>
-      <Container className="header-container">
-        <Navbar.Brand href="#" className="name-logo">
-          greku
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end links-container">
-          <a href="#home">Home</a>
-          <a href="#about">About</a>
-          <a href="#projects">Projects</a>
-          <a href="#contact">Contact</a>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar>
+        <Container className="header-container">
+          <Navbar.Brand href="#home" className="name-logo">
+            greku
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end links-container">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#projects">Projects</a>
+            <a href="#contact">Contact</a>
+          </Navbar.Collapse>
+          <HiOutlineMenuAlt3
+            className="mobile-dropdown-btn"
+            onClick={() => setShowMobileNav(!showMobileNav)}
+          />
+        </Container>
+      </Navbar>
+      <div className={showMobileNav ? "mobile-nav" : "mobile-nav hidden"}>
+        <a href="#about" onClick={() => setShowMobileNav(false)}>
+          About
+        </a>
+        <a href="#projects" onClick={() => setShowMobileNav(false)}>
+          Projects
+        </a>
+        <a href="#contact" onClick={() => setShowMobileNav(false)}>
+          Contact
+        </a>
+      </div>
+    </>
   );
 };
 
