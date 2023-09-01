65eb24198a8cdd986fd4daf1fbd2d21351b2f3f4
grekuu <gracjanprusik@gmail.com>
2023-09-01 15:03:24 +0200

    fix: deployment issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import About from "./components/About/About";
-import Contact from "./components/Contact/contact";
+import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
