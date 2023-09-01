b15630c468e67e6e50c00cb0cef914eb56aa6b15
grekuu <gracjanprusik@gmail.com>
2023-09-01 10:37:30 +0200

    feat(Footer): desktop component done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,25 @@
+import { Container } from "react-bootstrap";
 import "./footer.scss";
+import { AiFillLinkedin, AiOutlineGithub } from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="footer-container">
+      <Container className="content-container">
+        <div className="copyright">©{currentYear} All rights reserved.</div>
+        <div className="socials">
+          <a href="https://github.com/grekuu">
+            <AiOutlineGithub />
+          </a>
+          <a href="https://www.linkedin.com/in/gracjan-prusik-b12041251/">
+            <AiFillLinkedin />
+          </a>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
