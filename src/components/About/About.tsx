c7f591957e2342003a7dc9165cdcc3402678ee2a
grekuu <gracjanprusik@gmail.com>
2023-09-11 20:15:41 +0200

    feat: info update

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -12,13 +12,14 @@ const About = () => {
                     <h3>ABOUT ME</h3>
                     <h2>A dedicated Front-end Developer based in Szczecin, Poland 📍</h2>
                     <p>
-                        As a Junior Front-End Developer, I come equipped with a diverse toolkit encompassing HTML, CSS,
-                        JavaScript, React, Typescript, Bootstrap, React Toolkit and Sass. My strength shines through
-                        curating and upkeeping responsive websites that provide a seamless user journey. My craft
-                        revolves around forging interactive, captivating interfaces by scripting meticulous and
-                        streamlined code. I harness the power of advanced development tools and methodologies to create
-                        innovative digital experiences. Moreover, I thrive in synergistic collaborations, leveraging
-                        collective strengths to deliver exceptional web applications.
+                        As a Front-End Developer, I come equipped with a diverse toolkit encompassing HTML, CSS,
+                        JavaScript, React, Typescript, Bootstrap, Redux Toolkit and Sass and almost{' '}
+                        <b>a year of proven commercial experience</b>. My strength shines through curating and upkeeping
+                        responsive websites that provide a seamless user journey. My craft revolves around forging
+                        interactive, captivating interfaces by scripting meticulous and streamlined code. I harness the
+                        power of advanced development tools and methodologies to create innovative digital experiences.
+                        Moreover, I thrive in synergistic collaborations, leveraging collective strengths to deliver
+                        exceptional web applications.
                     </p>
                 </div>
             </Container>
